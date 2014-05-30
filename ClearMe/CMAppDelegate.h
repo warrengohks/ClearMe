08413531c6d25981ffd6bb884386778cc9db616f
Joshua Howland <development@jkhowland.com>
2014-05-30 01:38:37 -0600

    Initial Commit

---
+++ ClearMe/CMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMAppDelegate.h
+//  ClearMe
+//
+//  Created by Joshua Howland on 5/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClearMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClearMe
+//
+//  Created by Joshua Howland on 5/30/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMAppDelegate class]));
+    }
+}
